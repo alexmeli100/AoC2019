1cbbf61c5c2636b61b86d239e9a5eaba83088946
Alex <alexmeli100@gmail.com>
2019-12-15 01:40:39 -0500

    Finished day 12

--- day12/solution/src/main.kt
+++ day12/solution/src/main.kt
@@ -1,7 +1,7 @@
 import java.io.File
 import kotlin.math.*
 
-class Velocity(private var coords: IntArray): Iterable<Int>{
+class Velocity(var coords: IntArray): Iterable<Int>{
     operator fun get(i: Int) = coords[i]
     operator fun set(i: Int, value: Int) {
         coords[i] = value
@@ -11,7 +11,7 @@ class Velocity(private var coords: IntArray): Iterable<Int>{
     override fun toString(): String = String.format("vel=<x=${coords[0]}, y=${coords[1]}, z=${coords[2]}>")
 }
 
-class Pos(private var axis: IntArray): Iterable<Int> {
+class Pos(var axis: IntArray): Iterable<Int> {
     operator fun plus(v: Velocity) {
         axis.zip(v).map { (x1, y1) -> x1+y1 }.forEachIndexed{ i, a -> axis[i] = a }
     }
@@ -21,7 +21,7 @@ class Pos(private var axis: IntArray): Iterable<Int> {
 
 }
 
-class Moon(private var pos: Pos, private var vel: Velocity) {
+class Moon(var pos: Pos, var vel: Velocity) {
     fun applyGravity(other: Moon) {
         pos.zip(other.pos).forEachIndexed{ i, (x1, x2) ->
             val (a, b) = when(x1.compareTo(x2)) {
@@ -44,7 +44,7 @@ class Moon(private var pos: Pos, private var vel: Velocity) {
     override fun toString() = String.format("$pos  $vel")
 }
 
-class Galaxy(private var moons: MutableList<Moon>) {
+class Galaxy(var moons: MutableList<Moon>) {
     private fun updateVel() {
         for (i in 0 until moons.size) {
             for (j in i+1 until moons.size) {
@@ -58,10 +58,37 @@ class Galaxy(private var moons: MutableList<Moon>) {
         moons.forEach { it.updatePos() }
     }
 
+    fun cycles(index: Int): Long {
+        val m = mutableListOf<Moon>()
+
+        moons.forEach {
+            val p = Pos(it.pos.axis.copyOf())
+            val v = Velocity(it.vel.coords.copyOf())
+            m.add(Moon(p, v))
+        }
+
+        val g = Galaxy(m)
+        var res = 1
+        g.update()
+        while (!g.moons.all { it.vel[index] == 0 }) {
+            g.update()
+            res += 1
+        }
+
+        //g.moons.forEach { println(it)}
+        return 2L * res
+    }
+
     fun moonEnergies() = moons.map{ it.totalEnergy() }.sum()
 }
 
 private val input by lazy { File("input.txt").readText() }
+
+fun part2(g: Galaxy) {
+    val c = (0..2).map { g.cycles(it) }.fold(1L) { acc, x -> lcm(acc, x) }
+
+    println(c)
+}
 
 fun main() {
     val pat = "[-]?\\d+".toRegex()
@@ -75,8 +102,13 @@ fun main() {
 
     val galaxy = Galaxy(moons)
 
-    for (i in 0 until 1000)
-        galaxy.update()
+//    for (i in 0 until 1000)
+//        galaxy.update()
+//
+//    println("Total Energy is ${galaxy.moonEnergies()}")
+    part2(galaxy)
 
-    println("Total Energy is ${galaxy.moonEnergies()}")
 }
+
+tailrec fun gcd(a: Long, b: Long): Long = if(a == 0L) abs(b) else gcd(b % a, a)
+fun lcm(a: Long, b: Long) = (a * b) / gcd(a, b)
